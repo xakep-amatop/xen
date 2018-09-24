9e1e48dac832745a4432650f221f9dbb1f8f2985
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2018-09-24 17:04:10 +0300

    xen/arm: Inject GSX irq to all subscribed guest domains
    We need to take care of injecting GSX irq to guest domains
    that run GPU, since host driver doesn't send corresponding event
    to guest driver anymore.
    
    The patch was done in a way to inject GSX irq only to guest domains
    that really required it. We assume, if domain config file contains
    corresponding irq number then most likely it will run GPU.
    
    [OT] Squashed with:
    xen/arm: Remove BUG_ON from remove_gsx_guest func
    xen/arm: Check for valid desc->action in remove_gsx_guest()
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/vgic.c
+++ xen/arch/arm/vgic.c
@@ -168,6 +168,9 @@ void register_vgic_ops(struct domain *d, const struct vgic_ops *ops)
    d->arch.vgic.handler = ops;
 }
 
+extern const int gsx_irq_num;
+extern void remove_gsx_guest(struct domain *d);
+
 void domain_vgic_free(struct domain *d)
 {
     int i;
@@ -176,6 +179,9 @@ void domain_vgic_free(struct domain *d)
     for ( i = 0; i < (d->arch.vgic.nr_spis); i++ )
     {
         struct pending_irq *p = spi_to_pending(d, i + 32);
+
+        if ( p->irq == gsx_irq_num )
+            remove_gsx_guest(d);
 
         if ( p->desc )
         {
