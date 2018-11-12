8e553a58b7701c666a8b72433e2f681e33c97aa2
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/arm: Implement PSCI system suspend
    The implementation consists of:
    -Adding PSCI system suspend call as new PSCI function
    -Trapping PSCI system_suspend HVC
    -Implementing PSCI system suspend call (virtual interface that allows
     guests to suspend themselves)
    
    The PSCI system suspend should be called by a guest from its boot
    VCPU. Non-boot VCPUs of the guest should be hot-unplugged using PSCI
    CPU_OFF call prior to issuing PSCI system suspend. Interrupts that
    are left enabled by the guest are assumed to be its wake-up interrupts.
    Therefore, a wake-up interrupt triggers the resume of the guest. Guest
    should resume regardless of the state of Xen (suspended or not).
    
    When a guest calls PSCI system suspend the respective domain will be
    suspended if the following conditions are met:
    1) Given resume entry point is not invalid
    2) Other (if any) VCPUs of the calling guest are hot-unplugged
    
    If the conditions above are met the calling domain is labeled as
    suspended and the calling VCPU is blocked. If nothing else wouldn't
    be done the suspended domain would resume from the place where it
    called PSCI system suspend. This is expected if processing of the PSCI
    system suspend call fails. However, in the case of success the calling
    guest should resume (continue execution after the wake-up) from the entry
    point which is given as the first argument of the PSCI system suspend
    call. In addition to the entry point, the guest expects to start within
    the environment whose state matches the state after reset. This means
    that the guest should find reset register values, MMU disabled, etc.
    Thereby, the context of VCPU should be 'reset' (as if the system is
    comming out of reset), the program counter should contain entry point,
    which is 1st argument, and r0/x0 should contain context ID which is 2nd
    argument of PSCI system suspend call. The context of VCPU is set
    accordingly when the PSCI system suspend is processed, so that nothing
    needs to be done on resume/wake-up path. However, in order to ensure that
    this context doesn't get overwritten by the scheduler when scheduling out
    this VCPU (would normally happen after the calling CPU is blocked), we need
    to check whether to return early from ctxt_switch_from().
    
    There are variables in domain structure to keep track of domain shutdown.
    One of existing shutdown reason is 'suspend' which this patch is using to
    track the suspend state of a domain. Those variables are used to determine
    whether to early return from ctxt_switch_from() or not.
    
    A suspended domain will resume after the Xen receives an interrupt which is
    targeted to the domain, unblocks the domain's VCPU, and schedules it in.
    When the VCPU is scheduled in, the VCPU context is already reset, and
    contains the right resume entry point in program counter that will be
    restored in ctxt_switch_to(). The only thing that needs to be done at this
    point is to clear the variables that marked the domain state as suspended.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

---
+++ xen/arch/arm/include/asm/suspend.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __ASM_ARM_SUSPEND_H__
+#define __ASM_ARM_SUSPEND_H__
+
+int32_t domain_suspend(register_t epoint, register_t cid);
+
+#endif
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ xen/arch/arm/suspend.c
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <xen/sched.h>
+#include <asm/cpufeature.h>
+#include <asm/event.h>
+#include <asm/psci.h>
+#include <asm/suspend.h>
+#include <asm/platform.h>
+#include <public/sched.h>
+
+static void vcpu_suspend_prepare(register_t epoint, register_t cid)
+{
+    struct vcpu *v = current;
+
+    v->arch.suspend_ep = epoint;
+    v->arch.suspend_cid = cid;
+}
+
+int32_t domain_suspend(register_t epoint, register_t cid)
+{
+    struct vcpu *v;
+    struct domain *d = current->domain;
+    bool is_thumb = epoint & 1;
+
+    dprintk(XENLOG_DEBUG,
+            "Dom%d suspend: epoint=0x%"PRIregister", cid=0x%"PRIregister"\n",
+            d->domain_id, epoint, cid);
+
+    /* THUMB set is not allowed with 64-bit domain */
+    if ( is_64bit_domain(d) && is_thumb )
+        return PSCI_INVALID_ADDRESS;
+
+    /* TODO: care about locking here */
+    /* Ensure that all CPUs other than the calling one are offline */
+    for_each_vcpu ( d, v )
+    {
+        if ( v != current && is_vcpu_online(v) )
+            return PSCI_DENIED;
+    }
+
+    /*
+     * Prepare the calling VCPU for suspend (save entry point into pc and
+     * context ID into r0/x0 as specified by PSCI SYSTEM_SUSPEND)
+     */
+    vcpu_suspend_prepare(epoint, cid);
+
+    /*
+     * Set the domain state to suspended (will be cleared when the domain
+     * resumes, i.e. VCPU of this domain gets scheduled in).
+     */
+    d->is_shut_down = 1;
+    d->shutdown_code = SHUTDOWN_suspend;
+
+    /* Disable watchdogs of this domain */
+    watchdog_domain_suspend(d);
+
+    /*
+     * The calling domain is suspended by blocking its last running VCPU. If an
+     * event is pending the domain will resume right away (VCPU will not block,
+     * but when scheduled in it will resume from the given entry point).
+     */
+    vcpu_block_unless_event_pending(current);
+
+    return PSCI_SUCCESS;
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
