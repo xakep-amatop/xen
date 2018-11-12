7a589b676dc30415c66fb459faac7f0e7030d450
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:33:00 +0200

    xen/arm: Save/restore context on suspend/resume
    The context of CPU general purpose and system control registers must be
    saved on suspend and restored on resume. This is implemented in
    prepare_resume_ctx and before the return from the hyp_resume function.
    The prepare_resume_ctx must be invoked just before the PSCI system suspend
    call is issued to the ATF. The prepare_resume_ctx must return a non-zero
    value so that the calling 'if' statement evaluates to true, causing the
    system suspend to be invoked. Upon resume, the context saved on suspend
    will be restored, including the link register. Therefore, after
    restoring the context, the control flow will return to the address
    pointed to by the saved link register, which is the place from which
    prepare_resume_ctx was called. To ensure that the calling 'if' statement
    does not again evaluate to true and initiate system suspend, hyp_resume
    must return a zero value after restoring the context.
    
    Note that the order of saving register context into cpu_context structure
    must match the order of restoring.
    
    Support for ARM32 is not implemented. Instead, compilation fails with a
    build-time error if suspend is enabled for ARM32.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    ---
    Changes in v6:
    - Rename hyp_suspend to prepare_resume_ctx
    - moved invocation of prepare_resume_ctx to commit which
      implements system_suspend call
    
    Changes in v4:
    - Produce build-time error for ARM32 when CONFIG_SYSTEM_SUSPEND is enabled
    - Use register_t instead of uint64_t in cpu_context structure

--- xen/arch/arm/include/asm/suspend.h
+++ xen/arch/arm/include/asm/suspend.h
@@ -3,9 +3,31 @@
 #ifndef __ASM_ARM_SUSPEND_H__
 #define __ASM_ARM_SUSPEND_H__
 
+#include <asm/types.h>
+
 #ifdef CONFIG_SYSTEM_SUSPEND
 
+#ifdef CONFIG_ARM_64
+struct cpu_context {
+    register_t callee_regs[12];
+    register_t sp;
+    register_t vbar_el2;
+    register_t vtcr_el2;
+    register_t vttbr_el2;
+    register_t tpidr_el2;
+    register_t mdcr_el2;
+    register_t hstr_el2;
+    register_t cptr_el2;
+    register_t hcr_el2;
+} __aligned(16);
+#else
+#error "Define cpu_context structure for arm32"
+#endif
+
+extern struct cpu_context cpu_context;
+
 void hyp_resume(void);
+int prepare_resume_ctx(struct cpu_context *ptr);
 
 #endif /* CONFIG_SYSTEM_SUSPEND */
 
---
+++ xen/arch/arm/suspend.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <asm/suspend.h>
+
+struct cpu_context cpu_context;
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
