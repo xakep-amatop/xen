2b11964e2a98b8a460e95ea79290e32536e6b44e
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/arm: Implement PSCI SYSTEM_SUSPEND call (host interface)
    Invoke PSCI SYSTEM_SUSPEND to finalize Xen's suspend sequence on ARM64 platforms.
    Pass the resume entry point (hyp_resume) as the first argument to EL3. The resume
    handler is currently a stub and will be implemented later in assembly. Ignore the
    context ID argument, as is done in Linux.
    
    Only enable this path when CONFIG_SYSTEM_SUSPEND is set and
    PSCI version is >= 1.0.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    ---
    Changes in v6:
    - move calling of call_psci_system_suspend to commit that
      implements system_suspend call
    
    Changes in v4:
      - select the appropriate PSCI SYSTEM_SUSPEND function ID based on platform
      - update comments and commit message to reflect recent changes
    
    Changes in v3:
      - return PSCI_NOT_SUPPORTED instead of a hardcoded 1 on ARM32
      - check PSCI version before invoking SYSTEM_SUSPEND in call_psci_system_suspend

---
+++ xen/arch/arm/include/asm/suspend.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __ASM_ARM_SUSPEND_H__
+#define __ASM_ARM_SUSPEND_H__
+
+#ifdef CONFIG_SYSTEM_SUSPEND
+
+void hyp_resume(void);
+
+#endif /* CONFIG_SYSTEM_SUSPEND */
+
+#endif /* __ASM_ARM_SUSPEND_H__ */
+
+ /*
+  * Local variables:
+  * mode: C
+  * c-file-style: "BSD"
+  * c-basic-offset: 4
+  * tab-width: 4
+  * indent-tabs-mode: nil
+  * End:
+  */
