64cbfa58af7935c47141a2b1c0560e8d63e3d712
Mykola Kvach <mykola_kvach@epam.com>
2018-11-12 13:32:00 +0200

    xen/arm: Implement PSCI SYSTEM_SUSPEND call for guests
    This patch adds support for the PSCI SYSTEM_SUSPEND function in the vPSCI
    (virtual PSCI) interface, allowing guests to request suspend via the PSCI
    v1.0 SYSTEM_SUSPEND call (both 32-bit and 64-bit variants).
    
    The implementation:
    - Adds SYSTEM_SUSPEND function IDs to PSCI definitions
    - Implements trapping and handling of SYSTEM_SUSPEND in vPSCI
    - Allows only non-hardware domains to invoke SYSTEM_SUSPEND; for the
      hardware domain, PSCI_NOT_SUPPORTED is returned to avoid halting the
      system in hwdom_shutdown() called from domain_shutdown
    - Ensures all secondary VCPUs of the calling domain are offline before
      allowing suspend due to PSCI spec
    
    GIC and virtual timer context must be saved when the domain suspends.
    This is done by moving the respective code in ctxt_switch_from
    before the return that happens if the domain suspended.
    
    Usage:
    
    For Linux-based guests, suspend can be initiated with:
        echo mem > /sys/power/state
    or via:
        systemctl suspend
    
    Resuming the guest is performed from control domain using:
          xl resume <domain>
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    ---
    Changes in V8:
    - GIC and virtual timer context must be saved when the domain suspends
    - rework locking
    - minor changes after code review
    
    Changes in V7:
    - add proper locking
    - minor changes after code review
    
    Changes in V6:
    - skip execution of ctxt_switch_from for vcpu that is in paused domain
    - add implementation of domain_resume without domain_pause
    - add helper function to determine if vcpu is suspended or not
    - ignore upper 32 bits of argument values when the domain is 64-bit
      and calls the SMC32 SYSTEM_SUSPEND function
    - cosmetic changes after review
    
    Changes in V5:
    - don't use standby mode, restore execution in a provided by guest point
    - move checking that all CPUs, except current one, are offline to after
      pausing the vCPUs
    - provide ret status from arch_domain_shutdown and handle it in
      domain_shutdown
    - adjust VPSCI_NR_FUNCS to reflect the number of newly added PSCI functions
    
    Changes in V4:
    Dropped all changes related to watchdog, domain is marked as shutting
    down in domain_shutdown and watchdog timeout handler won't trigger
    because of it.
    
    Previous versions included code to manage Xen watchdog timers during suspend,
    but this was removed. When a guest OS starts the Xen watchdog (either via the
    kernel driver or xenwatchdogd), it is responsible for managing that state
    across suspend/resume. On Linux, the Xen kernel driver properly stops the
    watchdog during suspend. However, when xenwatchdogd is used instead, suspend
    handling is incomplete, potentially leading to watchdog-triggered resets on
    resume. Xen leaves watchdog handling to the guest OS and its services.
    
    Dropped all changes related to VCPU context, because instead domain_shutdown
    is used, so we don't need any extra changes for suspending domain.
    
    Changes in V3:
    Dropped all domain flags and related code (which touched common functions like
    vcpu_unblock), keeping only the necessary changes for Xen suspend/resume, i.e.
    suspend/resume is now fully supported only for the hardware domain.
    Proper support for domU suspend/resume will be added in a future patch.
    This patch does not yet include VCPU context reset or domain context
    restoration in VCPU.

--- xen/arch/arm/vpsci.c
+++ xen/arch/arm/vpsci.c
@@ -10,27 +10,17 @@
 
 #include <public/sched.h>
 
-static int do_common_cpu_on(register_t target_cpu, register_t entry_point,
-                            register_t context_id)
+static int do_setup_vcpu_ctx(struct vcpu *v, register_t entry_point,
+                      register_t context_id)
 {
-    struct vcpu *v;
     struct domain *d = current->domain;
     struct vcpu_guest_context *ctxt;
     int rc;
     bool is_thumb = entry_point & 1;
-    register_t vcpuid;
-
-    vcpuid = vaffinity_to_vcpuid(target_cpu);
-
-    if ( (v = domain_vcpu(d, vcpuid)) == NULL )
-        return PSCI_INVALID_PARAMETERS;
 
     /* THUMB set is not allowed with 64-bit domain */
     if ( is_64bit_domain(d) && is_thumb )
         return PSCI_INVALID_ADDRESS;
-
-    if ( !test_bit(_VPF_down, &v->pause_flags) )
-        return PSCI_ALREADY_ON;
 
     if ( (ctxt = alloc_vcpu_guest_context()) == NULL )
         return PSCI_DENIED;
@@ -78,9 +68,30 @@ static int do_common_cpu_on(register_t target_cpu, register_t entry_point,
     if ( rc < 0 )
         return PSCI_DENIED;
 
-    vcpu_wake(v);
-
     return PSCI_SUCCESS;
+}
+
+static int do_common_cpu_on(register_t target_cpu, register_t entry_point,
+                            register_t context_id)
+{
+    int rc;
+    struct vcpu *v;
+    struct domain *d = current->domain;
+    register_t vcpuid;
+
+    vcpuid = vaffinity_to_vcpuid(target_cpu);
+
+    if ( (v = domain_vcpu(d, vcpuid)) == NULL )
+        return PSCI_INVALID_PARAMETERS;
+
+    if ( !test_bit(_VPF_down, &v->pause_flags) )
+        return PSCI_ALREADY_ON;
+
+    rc = do_setup_vcpu_ctx(v, entry_point, context_id);
+    if ( rc == PSCI_SUCCESS )
+        vcpu_wake(v);
+
+    return rc;
 }
 
 static int32_t do_psci_cpu_on(uint32_t vcpuid, register_t entry_point)
@@ -197,6 +208,48 @@ static void do_psci_0_2_system_reset(void)
     domain_shutdown(d,SHUTDOWN_reboot);
 }
 
+static int32_t do_psci_1_0_system_suspend(register_t epoint, register_t cid)
+{
+    int32_t rc;
+    struct vcpu *v;
+    struct domain *d = current->domain;
+
+    /* SYSTEM_SUSPEND is not supported for the hardware domain yet */
+    if ( is_hardware_domain(d) )
+        return PSCI_NOT_SUPPORTED;
+
+    /* Ensure that all CPUs other than the calling one are offline */
+    domain_lock(d);
+    for_each_vcpu ( d, v )
+    {
+        if ( v != current && is_vcpu_online(v) )
+        {
+            domain_unlock(d);
+            return PSCI_DENIED;
+        }
+    }
+    domain_unlock(d);
+
+    rc = domain_shutdown(d, SHUTDOWN_suspend);
+    if ( rc )
+        return PSCI_DENIED;
+
+    rc = do_setup_vcpu_ctx(current, epoint, cid);
+    if ( rc != PSCI_SUCCESS )
+    {
+        domain_resume_nopause(d);
+        return rc;
+    }
+
+    set_bit(_VPF_suspended, &current->pause_flags);
+
+    dprintk(XENLOG_DEBUG,
+            "Dom %u: SYSTEM_SUSPEND requested, epoint=%#lx, cid=%#lx\n",
+            d->domain_id, epoint, cid);
+
+    return rc;
+}
+
 static int32_t do_psci_1_0_features(uint32_t psci_func_id)
 {
     /* /!\ Ordered by function ID and not name */
@@ -214,6 +267,8 @@ static int32_t do_psci_1_0_features(uint32_t psci_func_id)
     case PSCI_0_2_FN32_SYSTEM_OFF:
     case PSCI_0_2_FN32_SYSTEM_RESET:
     case PSCI_1_0_FN32_PSCI_FEATURES:
+    case PSCI_1_0_FN32_SYSTEM_SUSPEND:
+    case PSCI_1_0_FN64_SYSTEM_SUSPEND:
     case ARM_SMCCC_VERSION_FID:
         return 0;
     default:
@@ -341,6 +396,24 @@ bool do_vpsci_0_2_call(struct cpu_user_regs *regs, uint32_t fid)
 
         perfc_incr(vpsci_features);
         PSCI_SET_RESULT(regs, do_psci_1_0_features(psci_func_id));
+        return true;
+    }
+
+    case PSCI_1_0_FN32_SYSTEM_SUSPEND:
+    case PSCI_1_0_FN64_SYSTEM_SUSPEND:
+    {
+        register_t epoint = PSCI_ARG(regs, 1);
+        register_t cid = PSCI_ARG(regs, 2);
+
+        if ( is_64bit_domain(current->domain) &&
+             fid == PSCI_1_0_FN32_SYSTEM_SUSPEND )
+        {
+            epoint &= GENMASK(31, 0);
+            cid &= GENMASK(31, 0);
+        }
+
+        perfc_incr(vpsci_system_suspend);
+        PSCI_SET_RESULT(regs, do_psci_1_0_system_suspend(epoint, cid));
         return true;
     }
 
