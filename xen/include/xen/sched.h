d50c69b3009769e08c99b74511a39d3d0df148f7
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/x86: Move freeze/thaw_domains into common files
    These functions will be reused by suspend/resume support for ARM.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>

--- xen/include/xen/sched.h
+++ xen/include/xen/sched.h
@@ -1052,6 +1052,9 @@ static inline struct vcpu *domain_vcpu(const struct domain *d,
     return vcpu_id >= d->max_vcpus ? NULL : d->vcpu[idx];
 }
 
+void freeze_domains(void);
+void thaw_domains(void);
+
 void cpu_init(void);
 
 /*
