f17cc65355998948e4732dfe0feb3de49417927a
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:33:00 +0200

    xen/arm: Save/restore context on suspend/resume
    The context of CPU general purpose and system control registers
    has to be saved on suspend and restored on resume. This is
    implemented in hyp_suspend and before the return from hyp_resume
    function. The hyp_suspend is invoked just before the PSCI system
    suspend call is issued to the ATF. The hyp_suspend has to return a
    non-zero value so that the calling 'if' statement evaluates to true,
    causing the system suspend to be invoked. Upon the resume, context
    saved on suspend will be restored, including the link register.
    Therefore, after restoring the context the control flow will
    return to the address pointed by the saved link register, which
    is the place from which the hyp_suspend was called. To ensure
    that the calling 'if' statement doesn't again evaluate to true
    and initiate system suspend, hyp_resume has to return a zero value
    after restoring the context.
    
    Note that the order of saving register context into cpu_context
    structure has to match the order of restoring.
    
    Support for ARM32 is not implemented. Instead, compilation fails with a
    build-time error if suspend is enabled for ARM32.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    ---
    Changes in v4:
    - produce build-time error for ARM32 when CONFIG_SYSTEM_SUSPEND is enabled
    - use register_t instead of uint64_t in cpu_context structure

--- xen/arch/arm/include/asm/suspend.h
+++ xen/arch/arm/include/asm/suspend.h
@@ -5,9 +5,29 @@
 
 #ifdef CONFIG_SYSTEM_SUSPEND
 
+#ifdef CONFIG_ARM_64
+struct cpu_context {
+    register_t callee_regs[12];
+    register_t sp;
+    register_t vbar_el2;
+    register_t vtcr_el2;
+    register_t vttbr_el2;
+    register_t tpidr_el2;
+    register_t mdcr_el2;
+    register_t hstr_el2;
+    register_t cptr_el2;
+    register_t hcr_el2;
+} __aligned(16);
+#else
+#error "Define cpu_context structure for arm32"
+#endif
+
+extern struct cpu_context cpu_context;
+
 int host_system_suspend(void);
 
 void hyp_resume(void);
+int hyp_suspend(struct cpu_context *ptr);
 
 #endif /* CONFIG_SYSTEM_SUSPEND */
 
