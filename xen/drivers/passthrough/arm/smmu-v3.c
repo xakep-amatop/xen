16599d3af2a23c4e21e6bc67332dfb5a7fab5439
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2021-10-20 00:02:01 +0300

    iommu/arm: Remove code duplication in all IOMMU drivers
    All IOMMU drivers on Arm perform almost the same generic actions in
    hwdom_init callback. Move this code to common arch_iommu_hwdom_init()
    to get rid of code duplication.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/drivers/passthrough/arm/smmu-v3.c
+++ xen/drivers/passthrough/arm/smmu-v3.c
@@ -3404,16 +3404,6 @@ static int arm_smmu_iommu_xen_domain_init(struct domain *d)
 
 static void __hwdom_init arm_smmu_iommu_hwdom_init(struct domain *d)
 {
-	/* Set to false options not supported on ARM. */
-	if (iommu_hwdom_inclusive)
-		printk(XENLOG_WARNING
-		"map-inclusive dom0-iommu option is not supported on ARM\n");
-	iommu_hwdom_inclusive = false;
-	if (iommu_hwdom_reserved == 1)
-		printk(XENLOG_WARNING
-		"map-reserved dom0-iommu option is not supported on ARM\n");
-	iommu_hwdom_reserved = 0;
-
 	arch_iommu_hwdom_init(d);
 }
 
