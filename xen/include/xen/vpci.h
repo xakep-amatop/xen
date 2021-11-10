14d9ebd77be22be89a1db844b1da1eea5c9e2bdb
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-11-10 11:41:43 +0200

    [SR-IOV] vpci: add SR-IOV support
    SR-IOV virtual functions have simplified configuration space such as
    Vendor ID is derived from the physical function and Device ID comes
    from SR-IOV extended capability.
    Emulate those, so we can provide VID/DID pair for guests which use PCI
    passthrough for SR-IOV virtual functions.
    
    Emulate guest BAR register values based on PF BAR values for VFs.
    This allows creating a guest view of the normal BAR registers and emulates
    the size and properties as it is done during PCI device enumeration by
    the guest.
    
    Note, that VFs ROM BAR is read-only and is all zeros, but VF may provide
    access to the PFs ROM via emulation and is not implemented.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/include/xen/vpci.h
+++ xen/include/xen/vpci.h
@@ -102,7 +102,13 @@ struct vpci {
          * is mapped into guest p2m) if there's a ROM BAR on the device.
          */
         bool rom_enabled      : 1;
-        /* FIXME: currently there's no support for SR-IOV. */
+        /*
+         * Vendor and Device IDs for this virtual function if applicable.
+         * This is always guest's view as VFs do not provide
+         * PCI_VENDOR_ID:PCI_DEVICE_ID pair in their configuration space
+         * and always return 0xffffffff by SR-IOV specification.
+         */
+        uint32_t vf_ven_dev_id;
     } header;
 
     /* MSI data. */
@@ -154,6 +160,7 @@ struct vpci {
     /* Guest SBDF of the device. */
     pci_sbdf_t guest_sbdf;
 #endif
+    struct vpci_bar vf_bars[PCI_SRIOV_NUM_BARS];
 #endif
 };
 
@@ -269,6 +276,17 @@ u64 vpci_arch_readq(unsigned long addr);
 
 void vpci_arch_writel(u32 data, unsigned long addr);
 void vpci_arch_writeq(u64 data, unsigned long addr);
+
+void vpci_bar_write(const struct pci_dev *pdev, unsigned int reg,
+                    uint32_t val, void *data);
+void vpci_guest_bar_write(const struct pci_dev *pdev, unsigned int reg,
+                          uint32_t val, void *data);
+uint32_t vpci_guest_bar_read(const struct pci_dev *pdev, unsigned int reg,
+                             void *data);
+void vpci_guest_cmd_write(const struct pci_dev *pdev, unsigned int reg,
+                          uint32_t cmd, void *data);
+void vpci_cmd_write(const struct pci_dev *pdev, unsigned int reg,
+                    uint32_t cmd, void *data);
 
 #endif /* __XEN__ */
 
