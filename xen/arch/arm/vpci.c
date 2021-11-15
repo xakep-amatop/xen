0f660da592b9099ad6bb9b899b7056c07dba6eae
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-11-15 09:11:09 +0200

    [INCOMPLETE IMPL] xen/arm: add vPCI MSI dump key handler
    Dump vPCI MSI information on 'M' press while in Xen console.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/arch/arm/vpci.c
+++ xen/arch/arm/vpci.c
@@ -13,6 +13,7 @@
  */
 #include <xen/sched.h>
 #include <xen/vpci.h>
+#include <xen/keyhandler.h>
 
 #include <asm/mmio.h>
 
@@ -246,6 +247,20 @@ unsigned int domain_vpci_get_num_mmio_handlers(struct domain *d)
 
     return count;
 }
+
+static void dump_msi(unsigned char key)
+{
+    printk("MSI information:\n");
+
+    vpci_dump_msi();
+}
+
+static int __init msi_setup_keyhandler(void)
+{
+    register_keyhandler('M', dump_msi, "dump MSI state", 1);
+    return 0;
+}
+__initcall(msi_setup_keyhandler);
 
 /*
  * Local variables:
