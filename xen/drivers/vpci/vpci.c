e59f73fe6228c1c59c8b01d380ce31fbbe98c2d4
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2022-01-31 16:17:26 +0200

    vpci: restrict unhandled read/write operations for guests
    A guest would be able to read and write those registers which are not
    emulated and have no respective vPCI handlers, so it will be possible
    for it to access the hardware directly.
    In order to prevent a guest from reads and writes from/to the unhandled
    registers make sure only hardware domain can access the hardware directly
    and restrict guests from doing so.
    
    Suggested-by: Roger Pau Monné <roger.pau@citrix.com>
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    
    ---
    Since v6:
    - do not use is_hwdom parameter for vpci_{read|write}_hw and use
      current->domain internally
    - update commit message
    New in v6
    Moved into another series

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -220,6 +220,10 @@ static uint32_t vpci_read_hw(pci_sbdf_t sbdf, unsigned int reg,
 {
     uint32_t data;
 
+    /* Guest domains are not allowed to read real hardware. */
+    if ( !is_hardware_domain(current->domain) )
+        return ~(uint32_t)0;
+
     switch ( size )
     {
     case 4:
@@ -260,9 +264,13 @@ static uint32_t vpci_read_hw(pci_sbdf_t sbdf, unsigned int reg,
     return data;
 }
 
-static void vpci_write_hw(pci_sbdf_t sbdf, unsigned int reg, unsigned int size,
-                          uint32_t data)
+static void vpci_write_hw(pci_sbdf_t sbdf, unsigned int reg,
+                          unsigned int size, uint32_t data)
 {
+    /* Guest domains are not allowed to write real hardware. */
+    if ( !is_hardware_domain(current->domain) )
+        return;
+
     switch ( size )
     {
     case 4:
