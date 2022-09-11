be693d74502441b635774d5a6887c694d171b335
Stefano Stabellini <stefano.stabellini@amd.com>
2022-09-11 19:44:00 +0300

    libxl: add support for emulated TPM on ARM
    QEMU gained support for running as IOREQ server to do device emulation
    for ARM guests. It is done using a "xenpv" emulated machine for aarch64
    targets (same name as the x86 "xenpv" machine for PV backends.) It
    supports both PV backends and IOREQs.
    
    Introduce a new xl config option "tpm" to enable TPM emulation via QEMU.
    We are not reusing the existing vtpm option because that is tied to a PV
    frontend and backend protocol that has nothing to do with the TPM
    emulation provided by QEMU. vTPM is configured via xenstore, while
    emulated TPM via the QEMU command line.
    
    If tpm=1 pass the right command line parameters to QEMU so that it can
    connect to swtpm and provide TPM emulation for Xen guests. Also if tpm=1
    request xenpv QEMU to be spawned.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- tools/libs/light/libxl_arm.c
+++ tools/libs/light/libxl_arm.c
@@ -931,6 +931,31 @@ static int make_vpci_node(libxl__gc *gc, void *fdt,
     return 0;
 }
 
+static int make_tpm_node(libxl__gc *gc, void *fdt,
+                         const struct arch_info *ainfo,
+                         struct xc_dom_image *dom)
+{
+    int res;
+
+    res = fdt_begin_node(fdt, "tpm_tis");
+    if (res) return res;
+
+    res = fdt_property_compat(gc, fdt, 1, "tcg,tpm-tis-mmio");
+    if (res) return res;
+
+    res = fdt_property_regs(gc, fdt, GUEST_ROOT_ADDRESS_CELLS,
+            GUEST_ROOT_SIZE_CELLS, 1, GUEST_TPM_BASE, GUEST_TPM_SIZE);
+    if (res) return res;
+
+    res = fdt_property_string(fdt, "status", "okay");
+    if (res) return res;
+
+    res = fdt_end_node(fdt);
+    if (res) return res;
+
+    return 0;
+}
+
 static int make_xen_iommu_node(libxl__gc *gc, void *fdt)
 {
     int res;
@@ -1426,6 +1451,9 @@ next_resize:
                                               virtio->backend_domid) );
         }
 
+        if (libxl_defbool_val(d_config->b_info.tpm))
+            FDT( make_tpm_node(gc, fdt, ainfo, dom) );
+
         /*
          * The iommu node should be created only once for all virtio-mmio
          * devices.
@@ -1729,6 +1757,7 @@ int libxl__arch_domain_build_info_setdefault(libxl__gc *gc,
 {
     /* ACPI is disabled by default */
     libxl_defbool_setdefault(&b_info->acpi, false);
+    libxl_defbool_setdefault(&b_info->tpm, false);
 
     if (b_info->type != LIBXL_DOMAIN_TYPE_PV)
         return 0;
