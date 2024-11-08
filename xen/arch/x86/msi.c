36b043a84a20e11b9b8e09f4d8059848bbb44d25
Andrew Cooper <andrew.cooper3@citrix.com>
2024-11-08 18:54:47 +0000

    x86/mach-apic: Move the genapic wrappers to genapic.h
    This a better place for them to live.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/msi.c
+++ xen/arch/x86/msi.c
@@ -22,12 +22,15 @@
 #include <xen/keyhandler.h>
 #include <xen/pfn.h>
 #include <xen/msi.h>
+#include <asm/apic.h>
+#include <asm/genapic.h>
 #include <asm/io.h>
+#include <asm/io_apic.h>
 #include <asm/smp.h>
 #include <asm/desc.h>
 #include <asm/fixmap.h>
 #include <asm/p2m.h>
-#include <mach_apic.h>
+
 #include <io_ports.h>
 #include <irq_vectors.h>
 #include <public/physdev.h>
