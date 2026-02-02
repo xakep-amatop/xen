533771ff75bfad81955da15bdf86ad2c6d0e98e6
Mykyta Poturai <Mykyta_Poturai@epam.com>
2026-02-02 16:14:37 +0000

    arm/gicv4-its: Add GICv4 ITS command definitions
    Add definitions for GICv4 specific ITS commands.
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Message-ID: <3eb1cea5fb870e01b86e964caef5ceb9b2a8ed75.1770046465.git.mykyta_poturai@epam.com>

---
+++ xen/arch/arm/include/asm/gic_v4_its.h
@@ -0,0 +1,41 @@
+/*
+ * ARM GICv4 ITS support
+ *
+ * Penny Zheng <penny.zheng@arm.com>
+ * Copyright (c) 2023 ARM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; under version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASM_ARM_GICV4_ITS_H__
+#define __ASM_ARM_GICV4_ITS_H__
+
+#define GITS_CMD_VMOVI                   0x21
+#define GITS_CMD_VMOVP                   0x22
+#define GITS_CMD_VSGI                    0x23
+#define GITS_CMD_VSYNC                   0x25
+#define GITS_CMD_VMAPP                   0x29
+#define GITS_CMD_VMAPTI                  0x2a
+#define GITS_CMD_VINVALL                 0x2d
+#define GITS_CMD_INVDB                   0x2e
+
+#endif
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
