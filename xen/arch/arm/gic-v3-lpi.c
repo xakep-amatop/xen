bcc4afcf926b0a140640142550ca701fe828276d
Mykyta Poturai <mykyta_poturai@epam.com>
2026-04-06 12:05:14 +0300

    arm/its: Add LPI property table helpers
    Add accessors for the host LPI property table and maximum host LPI
    count, and provide an lpi_write_config() helper for updating LPI
    configuration entries.
    
    This keeps lpi_data private to gic-v3-lpi.c instead of exporting the
    whole structure in the header.
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>
    ---
    Changes compared to RFC:
    - code cleanup
    - keep lpi_data private to gic-v3-lpi.c
    - add helper accessors instead of exporting the whole struct
    (cherry picked from commit 87275ee9fc1ca4292acad833d278f68dde47329b)

--- xen/arch/arm/gic-v3-lpi.c
+++ xen/arch/arm/gic-v3-lpi.c
@@ -493,6 +493,35 @@ static int find_unused_host_lpi(uint32_t start, uint32_t *index)
     return -1;
 }
 
+uint8_t *lpi_host_proptable(void)
+{
+    return lpi_data.lpi_property;
+}
+
+unsigned long lpi_max_host_lpis(void)
+{
+    return lpi_data.max_host_lpi_ids;
+}
+
+int lpi_write_config(uint8_t *prop_table, uint32_t lpi, uint8_t clr,
+                     uint8_t set)
+{
+    u8 *cfg;
+
+    if ( !is_lpi(lpi) || lpi - LPI_OFFSET >= MAX_NR_HOST_LPIS )
+        return -ERANGE;
+
+    cfg = prop_table + lpi - LPI_OFFSET;
+    *cfg &= ~clr;
+    *cfg |= set | LPI_PROP_RES1;
+
+    /* Make the above write visible to the redistributors. */
+    if ( lpi_data.flags & LPI_PROPTABLE_NEEDS_FLUSHING )
+        clean_and_invalidate_dcache_va_range(cfg, sizeof(*cfg));
+
+    return 0;
+}
+
 /*
  * Allocate a block of 32 LPIs on the given host ITS for device "devid",
  * starting with "eventid". Put them into the respective ITT by issuing a
