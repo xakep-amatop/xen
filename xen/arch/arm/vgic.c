2fceffa423c9126ed6637d4f0999e11690279639
Mykola Kvach <mykola_kvach@epam.com>
2026-04-09 00:21:39 +0300

    arm/vgic: Allocate local pending IRQ state separately
    The GICv4 changes grow the per-VCPU local pending_irq array enough to
    push struct vcpu past PAGE_SIZE.
    
    Keep only a pointer in struct vgic_cpu and allocate the NR_LOCAL_IRQS
    array during vcpu_vgic_init()/free. With struct vcpu back under one
    page, restore the single-page allocator and reinstate the build-time
    check in common/domain.c.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- xen/arch/arm/vgic.c
+++ xen/arch/arm/vgic.c
@@ -374,20 +374,24 @@ int vcpu_vgic_init(struct vcpu *v)
 
     v->arch.vgic.private_irqs = xzalloc(struct vgic_irq_rank);
     if ( v->arch.vgic.private_irqs == NULL )
-      return -ENOMEM;
+        return -ENOMEM;
+
+    v->arch.vgic.pending_irqs =
+        xzalloc_array(struct pending_irq, NR_LOCAL_IRQS);
+    if ( v->arch.vgic.pending_irqs == NULL )
+    {
+        ret = -ENOMEM;
+        goto free_private_irqs;
+    }
 
     /* SGIs/PPIs are always routed to this VCPU */
     vgic_rank_init(v->arch.vgic.private_irqs, 0, v->vcpu_id);
 
     ret = v->domain->arch.vgic.handler->vcpu_init(v);
     if ( ret )
-    {
-        XFREE(v->arch.vgic.private_irqs);
-        return ret;
-    }
+        goto free_pending_irqs;
 
-    memset(&v->arch.vgic.pending_irqs, 0, sizeof(v->arch.vgic.pending_irqs));
-    for (i = 0; i < 32; i++)
+    for ( i = 0; i < NR_LOCAL_IRQS; i++ )
         vgic_init_pending_irq(&v->arch.vgic.pending_irqs[i], i);
 
     INIT_LIST_HEAD(&v->arch.vgic.inflight_irqs);
@@ -395,10 +399,18 @@ int vcpu_vgic_init(struct vcpu *v)
     spin_lock_init(&v->arch.vgic.lock);
 
     return 0;
+
+ free_pending_irqs:
+    XFREE(v->arch.vgic.pending_irqs);
+ free_private_irqs:
+    XFREE(v->arch.vgic.private_irqs);
+
+    return ret;
 }
 
 void vcpu_vgic_free(struct vcpu *v)
 {
+    XFREE(v->arch.vgic.pending_irqs);
     XFREE(v->arch.vgic.private_irqs);
 }
 
